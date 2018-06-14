032c1b2ed6e345cece591408d47a96f8589ca62b
huskyhoochu <dfg1499@gmail.com>
2018-06-15 02:24:52 +0900

    부트스트랩 및 디자인 템플릿 CSS 임포트

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react';
+
+// Bootstrap 3.2.0
+import 'bootstrap/dist/js/bootstrap.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Third-Party CSS
+import './static/css/bootstrap-modal.css';
+import './static/css/owl.carousel.css';
+
+// 제작자 자체 CSS
+import './static/css/sidewaze_base.css';
+import './static/css/sidewaze_columns.css';
+
 import NavigationColumn from './components/navigation_column';
 
 class App extends Component {
