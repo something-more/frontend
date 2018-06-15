4111a24ebc7f085f50a23c277cef8573bd167c1e
huskyhoochu <dfg1499@gmail.com>
2018-06-15 15:08:04 +0900

    로고 컬럼에 이미지 삽입, 소셜 링크 조정

--- src/components/logo_column.js
+++ src/components/logo_column.js
@@ -1,32 +1,51 @@
 import React from 'react';
+import logo from '../static/images/text_logo.svg';
 
 export default () => {
   return (<div className="logo-col">
-    <a href="#" className="toggle-menu"><i className="fa fa-bars"></i></a>
+    <a href="#" className="toggle-menu">
+      <i className="fa fa-bars"/></a>
     <div className="logo-wrapper">
-      <a href="#" className="clearfix"> <img id="logo" src="assets/images/logo.png" alt="company name"></img></a>
+      <a href="/" className="clearfix">
+        <img
+        id="logo"
+        src={logo}
+        alt="company name"/>
+      </a>
     </div>
     <div className="search-wrapper">
-      <a href="#"
+      <a
+      href="#"
       className="toggle-search tooltip-hover"
       title="search"
-      data-placement="left"><i className="fa fa-search"></i></a>
+      data-placement="left">
+        <i className="fa fa-search"/>
+      </a>
       <div className="search-panel">
         <form role="form">
-          <input type="search" placeholder="Enter search term and press enter" className="form-control"/>
+          <input
+          type="search"
+          placeholder="Enter search term and press enter"
+          className="form-control"/>
         </form>
       </div>
     </div>
     <div className="social-wrapper">
       <ul>
-        <li><a href="#"
-        className="fa fa-facebook tooltip-hover"
-        data-placement="left"
-        title="Follow Us on Facebook"></a></li>
-        <li><a href="#"
-        className="fa fa-instagram tooltip-hover"
-        data-placement="left"
-        title="Follow Us on Instagram"></a></li>
+        <li>
+          <a
+          href="#"
+          className="fa fa-facebook tooltip-hover"
+          data-placement="left"
+          target="_blank"
+          title="Follow Us on Facebook"/></li>
+        <li>
+          <a
+          href="https://www.instagram.com/_somethingmore/"
+          className="fa fa-instagram tooltip-hover"
+          data-placement="left"
+          target="_blank"
+          title="Follow Us on Instagram"/></li>
       </ul>
     </div>
   </div>
