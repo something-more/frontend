1f46ece99991f0e62bfc700b693a25aa0b40d233
huskyhoochu <dfg1499@gmail.com>
2018-06-16 16:53:35 +0900

    react-router-dom 요소 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Bootstrap 3.2.0
 import 'bootstrap/dist/js/bootstrap.min';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './static/css/bootstrap.min.css';
 
 // Third-Party CSS
 import './static/css/bootstrap-modal.css';
@@ -14,18 +15,30 @@ import './static/css/sidewaze_columns.css';
 import './static/css/sidewaze_base_color.css';
 import './static/css/soft-gray.css';
 
-// Components
+// Static Components
+import NavSkip from './components/nav_skip';
 import NavigationColumn from './components/navigation_column';
 import LogoColumn from './components/logo_column';
 import FooterColumn from './components/footer_column';
+import ToTop from './components/to_top';
+
+// Dynamic Components
+import PostsIndex from './components/posts_index';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavSkip/>
         <NavigationColumn/>
         <LogoColumn/>
+        <BrowserRouter>
+          <Switch>
+            <Route to="/" component={PostsIndex}/>
+          </Switch>
+        </BrowserRouter>
         <FooterColumn/>
+        <ToTop/>
       </div>
     );
   }
