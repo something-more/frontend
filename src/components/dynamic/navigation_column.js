8d1a5d6bc3ed2b9c7086cabca315800bf09acde0
huskyhoochu <dfg1499@gmail.com>
2018-06-20 00:15:18 +0900

    리덕스 스테이트를 이용한 author 리스트 동적 렌더링

--- src/components/dynamic/navigation_column.js
+++ src/components/dynamic/navigation_column.js
@@ -1,40 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import AuthorDetail from '../dynamic/author_detail';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { selectAuthor } from '../../reducers/reducer_active_author';
 
-export default () => {
-  return (
-  <div className="nav-col" style={{"border": "none"}}>
-    <nav id="nav" className="clearfix" role="navigation">
-      <div className="user-wrapper">
-        <p>
-          <a href="#login-modal" data-toggle="modal" className="toggle-login">
-            <i className="fa fa-sign-in"/>&nbsp;&nbsp;Login</a>
-          <span className="v-divider"/>
-          <a href="#signup-modal" data-toggle="modal" className="toggle-signup">
-            <i className="fa fa-pencil"/>&nbsp;&nbsp;Sign Up</a>
-        </p>
-      </div>
-      <ul className="primary-nav">
-        <li className="active">
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li className="has-children">
-          <a>Editors</a>
-          <ul>
-            <li><Link to="/authors">Kim Hojang</Link></li>
-            <li><a href="#">Han Siwon</a></li>
-            <li><a href="#">Seung Hyung Soo</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">Stories</a>
-        </li>
-      </ul>
-    </nav>
-  </div>
-  )
+class Navigation extends Component {
+  renderAuthors() {
+    return this.props.authors.map((author) => {
+      return (
+      <li key={author.id}>
+        <Link to={`/authors/${author.id}`}>
+        {author.name_en}
+        </Link>
+      </li>
+      )
+    })
+  }
+
+  render() {
+    return (
+    <div className="nav-col" style={{"border": "none"}}>
+      <nav id="nav" className="clearfix" role="navigation">
+        <div className="user-wrapper">
+          <p>
+            <a href="#login-modal" data-toggle="modal" className="toggle-login">
+              <i className="fa fa-sign-in"/>&nbsp;&nbsp;Login</a>
+            <span className="v-divider"/>
+            <a href="#signup-modal" data-toggle="modal" className="toggle-signup">
+              <i className="fa fa-pencil"/>&nbsp;&nbsp;Sign Up</a>
+          </p>
+        </div>
+        <ul className="primary-nav">
+          <li className="active">
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li className="has-children">
+            <a>Editors</a>
+            <ul>
+              {this.renderAuthors()}
+            </ul>
+          </li>
+          <li>
+            <a href="#">Stories</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    )
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    authors: state.authors
+  }
+}
+
+export default connect(mapStateToProps)(Navigation);
