c142cca0a340737866a3cb97ba421fa4ef4dcfe3
huskyhoochu <dfg1499@gmail.com>
2018-06-20 00:14:37 +0900

    리덕스 스테이트 추가

--- src/components/dynamic/author_detail.js
+++ src/components/dynamic/author_detail.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectAuthor } from '../../reducers/reducer_active_author';
 
 class AuthorDetail extends Component {
+  componentDidMount() {
+    const {id} = this.props.match.params;
+    this.props.selectAuthor(id);
+  }
+
   render() {
     return (
     <div className="content-col">
-      Author!
+      <p>이름: {this.props.author.name_ko}</p>
     </div>
     )
   }
 }
 
-export default AuthorDetail;
+function mapStateToProps(state) {
+  return {
+    author: state.authors[this.props.match.params.id -1]
+  };
+}
+
+export default connect(mapStateToProps, {selectAuthor})(AuthorDetail);
