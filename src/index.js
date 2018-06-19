990c8357ca5f28856905cf2baf3e938dbf039850
huskyhoochu <dfg1499@gmail.com>
2018-06-20 00:14:02 +0900

    리덕스 미들웨어 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './App';
+import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+<Provider store={createStoreWithMiddleware(reducers)}>
+  <App/>
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
