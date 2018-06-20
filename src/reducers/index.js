786435f9c80e63752fb964c6ef1b16f5f0d2ce9b
huskyhoochu <dfg1499@gmail.com>
2018-06-20 20:04:07 +0900

    formReducer와 authReducer 추가

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import Authors from './reducer_authors';
 import ActiveAuthor from './reducer_active_author';
+import AuthReducer from './reducer_auth';
 
 const rootReducer = combineReducers({
   authors: Authors,
-  activeAuthor: ActiveAuthor
+  activeAuthor: ActiveAuthor,
+  auth: AuthReducer,
+  form: formReducer
 });
 
 export default rootReducer;
