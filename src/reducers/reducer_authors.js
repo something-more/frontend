261244200128144c52db4bfdc784123c337107d0
huskyhoochu <dfg1499@gmail.com>
2018-06-20 14:31:23 +0900

    작가 정보에 썸네일 항목 추가

--- src/reducers/reducer_authors.js
+++ src/reducers/reducer_authors.js
@@ -1,8 +1,25 @@
+import hyung_soo from '../assets/images/thumbnail/hyung_soo_thumb-min.jpg';
+
 export default function () {
  return [
-   {id: 1, name_ko: '김호장', name_en: 'Kim Hojang'},
-   {id: 2, name_ko: '한시원', name_en: 'Han Siwon'},
-   {id: 3, name_ko: '승형수', name_en: 'Seung Hyung Soo'},
+   {
+     id: 1,
+     name_ko: '김호장',
+     name_en: 'Kim Hojang',
+     thumbnail: ''
+   },
+   {
+     id: 2,
+     name_ko: '한시원',
+     name_en: 'Han Siwon',
+     thumbnail: ''
+   },
+   {
+     id: 3,
+     name_ko: '승형수',
+     name_en: 'Seung Hyung Soo',
+     thumbnail: hyung_soo
+   },
  ]
 }
 
