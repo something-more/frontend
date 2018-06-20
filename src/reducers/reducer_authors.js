bcebb525e9a8b3ac8f112db1567de1061979dc6f
huskyhoochu <dfg1499@gmail.com>
2018-06-20 15:24:57 +0900

    필진 정보 추가: 이메일, 소개 문구

--- src/reducers/reducer_authors.js
+++ src/reducers/reducer_authors.js
@@ -6,19 +6,25 @@ export default function () {
      id: 1,
      name_ko: '김호장',
      name_en: 'Kim Hojang',
-     thumbnail: ''
+     thumbnail: '',
+     introduce: '',
+     email: 'great-ho@somethingmore.co.kr'
    },
    {
      id: 2,
      name_ko: '한시원',
      name_en: 'Han Siwon',
-     thumbnail: ''
+     thumbnail: '',
+     introduce: '',
+     email: ''
    },
    {
      id: 3,
      name_ko: '승형수',
      name_en: 'Seung Hyung Soo',
-     thumbnail: hyung_soo
+     thumbnail: hyung_soo,
+     introduce: '컴퓨터의 말과 인간의 말을 함께 배워갑니다.',
+     email: 'huskyhoochu@somethingmore.co.kr'
    },
  ]
 }
