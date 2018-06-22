ab1f4289a835359ec12046537b9fa2ec8743d260
huskyhoochu <dfg1499@gmail.com>
2018-06-22 19:23:48 +0900

    author reducer 간소화

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import Authors from './reducer_authors';
-import ActiveAuthor from './reducer_active_author';
+import AuthorReducer from './reducer_author';
 import AuthReducer from './reducer_auth';
 
 const rootReducer = combineReducers({
-  authors: Authors,
-  activeAuthor: ActiveAuthor,
+  author: AuthorReducer,
   auth: AuthReducer,
   form: formReducer
 });
