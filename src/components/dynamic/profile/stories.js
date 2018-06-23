e3be83b9b11bda6357afaca2589b9bb95146f784
huskyhoochu <dfg1499@gmail.com>
2018-06-23 19:35:10 +0900

    nav tab 구현

--- src/components/dynamic/profile/stories.js
+++ src/components/dynamic/profile/stories.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 
+const $ = window.jQuery;
+
 class Stories extends Component {
+  componentWillMount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+  }
+
+  componentWillUnmount() {
+    document.addEventListener('mousedown', this.handleClick, false);
+  }
+
+  handleClick = () => {
+    $('#myTabs a').click(function (event) {
+      event.preventDefault();
+      $(this).tab('show');
+    });
+  };
+
   render() {
     return (
-    <div className="content-col">
-      <div
-        className="row"
-        style={{
-          margin: "50px 0 100px 0",
-          padding: "0 10px"}}>
-        <div className="col">
-          <h1 className="title">Dashboard</h1>
-        </div>
+    <div ref={node => this.node = node}
+      className="content-col">
+      <div className="inner-content">
+        <h1 className="title">Stories</h1>
+        <hr className="vertical-spacer"/>
+        <button style={{color: "#ec5004"}} className="btn btn-link pull-right">Let's Post</button>
+        <ul id="myTabs" className="nav nav-tabs">
+          <li className="active"><a href="#">Published</a></li>
+          <li><a href="#">Draft</a></li>
+        </ul>
       </div>
     </div>
     )
