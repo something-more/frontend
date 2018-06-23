88e3f04ce605667710732c383b3cd37b1d2c5fa5
huskyhoochu <dfg1499@gmail.com>
2018-06-23 13:54:35 +0900

    인증 상태에 따른 렌더링 변경 함수 추가

--- src/components/dynamic/navigation_column.js
+++ src/components/dynamic/navigation_column.js
@@ -2,8 +2,40 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { selectAuthor } from '../../reducers/reducer_author';
+import { signOut } from '../../reducers/reducer_auth';
 
 class Navigation extends Component {
+
+  // 인증 상태 렌더링 함수
+  // sessionStorage 에 정보가 하나라도 없으면 로그인 / 회원가입 창을 띄우고
+  // 그렇지 않으면 로그인 되었음을 보여준다
+  renderAuthenticationState() {
+    if (sessionStorage.length !== 2) {
+      return (
+      <p>
+        <a href="#login-modal" data-toggle="modal" className="toggle-login">
+          <i className="fa fa-sign-in"/>&nbsp;&nbsp;Login</a>
+        <span className="v-divider"/>
+        <a href="#signup-modal" data-toggle="modal" className="toggle-signup">
+          <i className="fa fa-pencil"/>&nbsp;&nbsp;Sign Up</a>
+      </p>)
+    } else {
+      const retrieveData = JSON.parse(sessionStorage.getItem('user_data'));
+      return (
+        <p>
+          <span>{retrieveData.email}</span>
+          <span className="v-divider"/>
+          <Link
+            to={'/'}
+            className="toggle-login"
+            onClick={() => {this.props.signOut(); window.location.reload();}}>
+            <i className="fa fa-sign-out">&nbsp;&nbsp;Sign Out</i>
+          </Link>
+        </p>
+      )
+    }
+  }
+
   // 필진 리스트 렌더링 함수
   // 리스트를 클릭하면 액션 생성자를 호출해 특정 필진 데이터를 리듀서로 옮긴다
   renderAuthors() {
@@ -25,13 +57,7 @@ class Navigation extends Component {
     <div className="nav-col" style={{"border": "none"}}>
       <div id="nav" className="clearfix" role="navigation">
         <div className="user-wrapper">
-          <p>
-            <a href="#login-modal" data-toggle="modal" className="toggle-login">
-              <i className="fa fa-sign-in"/>&nbsp;&nbsp;Login</a>
-            <span className="v-divider"/>
-            <a href="#signup-modal" data-toggle="modal" className="toggle-signup">
-              <i className="fa fa-pencil"/>&nbsp;&nbsp;Sign Up</a>
-          </p>
+          {this.renderAuthenticationState()}
         </div>
         <ul className="primary-nav">
           <li className="active">
@@ -62,4 +88,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, { selectAuthor })(Navigation);
+export default connect(mapStateToProps, { selectAuthor, signOut })(Navigation);
