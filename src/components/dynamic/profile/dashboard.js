21e23aedfcd9934ac09ff0ae4c2fdea2e06373bf
huskyhoochu <dfg1499@gmail.com>
2018-06-23 17:38:09 +0900

    대시보드 컴포넌트 생성

---
+++ src/components/dynamic/profile/dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+    <div className="content-col">
+      hi
+    </div>
+    )
+  }
+}
+
+export default Dashboard;
