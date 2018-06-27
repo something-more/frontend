72c049700f00eb700fbeb67afccfda5aa6aa9df1
huskyhoochu <dfg1499@gmail.com>
2018-06-27 19:48:02 +0900

    어드민 설정 관리하는 리듀서 생성

---
+++ src/reducers/reducer_admin.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { axiosConfig } from '../include/axios';
+
+// axios 설정
+axiosConfig(axios);
+
+// Actions
+
+
+// Action Creators
+
+
+// Initial State
+
+
+// Reducer
+
+
+// Reducer Functions
