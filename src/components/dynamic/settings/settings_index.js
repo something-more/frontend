4516f25671734b55e3478e0408fca595e6a8f33d
huskyhoochu <dfg1499@gmail.com>
2018-06-27 17:29:42 +0900

    회원 탈퇴 기능 구현

--- src/components/dynamic/settings/settings_index.js
+++ src/components/dynamic/settings/settings_index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { destroyUser } from '../../../reducers/reducer_auth';
 import { decodeJWT } from '../../../include/jwt_decode';
 
 class SettingsIndex extends Component {
@@ -7,13 +10,61 @@ class SettingsIndex extends Component {
     status: ''
   };
 
-  renderSetPassword() {
+  errorAlert() {
+    // 에러가 발견되면 경고창 띄움
+    if (this.props.error) {
+      return (
+      <div className="alert alert-danger">
+        <button type="button" className="close" data-dismiss="alert">&times;</button>
+        <strong>{this.props.error}</strong>
+      </div>
+      )
+    }
+  }
+
+  async onDestroy(values) {
+    await this.props.destroyUser(values);
+
+    if (!this.props.error) {
+      await this.props.history.replace('/');
+      await window.location.reload();
+    }
+  }
+
+  renderSettings() {
+    const { handleSubmit } = this.props;
     switch(this.state.status) {
       case 'password':
         return (<div>password</div>);
 
       case 'destroy':
-        return (<div>destroy</div>);
+        return (
+        <div>
+          <h4>회원 탈퇴</h4>
+          <div className="panel panel-danger">
+            <div className="panel-heading">
+              <strong>정말로 탈퇴하시겠습니까? 이 작업은 되돌릴 수 없습니다.</strong>
+            </div>
+            <div className="panel-body">
+              <label htmlFor="destroyPassword">Password</label>
+              <form method="post" onSubmit={handleSubmit(this.onDestroy.bind(this))}>
+              <div className="input-group">
+                <Field type="password"
+                className="form-control"
+                id="destroyPassword"
+                placeholder="Password"
+                component="input"
+                name="password"/>
+                <span className="input-group-btn">
+                  <button className="btn btn-danger" type="submit">Submit</button>
+                </span>
+              </div>
+              </form>
+              <small>정말로 탈퇴하시려면 다시 한번 패스워드를 입력해 주십시오.</small>
+            </div>
+          </div>
+        </div>
+        );
 
       default:
         return null
@@ -29,6 +80,7 @@ class SettingsIndex extends Component {
           <h1 className="title">Settings</h1>
           <div className="row">
             <div className="col-md-6">
+              <h4>회원 정보</h4>
               <ul className="list-group">
                 <li className="list-group-item active">
                   <strong>Email:&nbsp;</strong>
@@ -49,7 +101,9 @@ class SettingsIndex extends Component {
               </ul>
             </div>
             <div className="col-md-6">
-              {this.renderSetPassword()}
+              {this.renderSettings()}
+              <hr className="vertical-spacer"/>
+              {this.errorAlert()}
             </div>
           </div>
         </div>
@@ -58,4 +112,14 @@ class SettingsIndex extends Component {
   }
 }
 
-export default SettingsIndex;
+function mapStateToProps(state) {
+  return {
+    error: state.auth.error
+  }
+}
+
+export default reduxForm({
+  form: 'UserSettingForm'
+})(
+  connect(mapStateToProps, { destroyUser })(SettingsIndex)
+);
