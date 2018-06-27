5d5a9c1388a9e8565787791c3904a958b9cc69c4
huskyhoochu <dfg1499@gmail.com>
2018-06-27 20:45:33 +0900

    어드민 리듀서 생성 및 유저 리스트 호출 액션 생성

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
+import AdminReducer from './reducer_admin';
 import AuthorReducer from './reducer_author';
 import AuthReducer from './reducer_auth';
 import StoryReducer from './reducer_story';
 
 const rootReducer = combineReducers({
+  admin: AdminReducer,
   author: AuthorReducer,
   auth: AuthReducer,
   story: StoryReducer,
--- src/reducers/reducer_admin.js
+++ src/reducers/reducer_admin.js
@@ -5,15 +5,65 @@ import { axiosConfig } from '../include/axios';
 axiosConfig(axios);
 
 // Actions
-
+const LIST = 'something-more/admin/LIST';
 
 // Action Creators
 
+// 유저 리스트
+export async function listUsers() {
+
+  let response, error = '';
+
+  try {
+    response = await axios({
+      method: 'get',
+      url: '/users/',
+      headers: {
+        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+      }
+    });
+  } catch (e) {
+    error = e.response.data.message;
+  }
+
+  return {
+    type: LIST,
+    response: response,
+    error: error
+  }
+}
 
 // Initial State
-
+const initialState = {
+  list: [],
+  retrieve: {},
+  error: ''
+};
 
 // Reducer
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case LIST:
+      return reducerListUsers(state, action);
 
+    default:
+      return state;
+  }
+}
 
 // Reducer Functions
+function reducerListUsers(state, action) {
+  if (action.response) {
+    return {
+      ...state,
+      list: action.response.data,
+      error: ''
+    }
+  } else {
+    return {
+      ...state,
+      list: [],
+      error: action.error
+    }
+  }
+}
