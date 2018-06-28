01601d1109d5714ddeaecdc274f94a7139c348db
huskyhoochu <dfg1499@gmail.com>
2018-06-28 16:55:08 +0900

    자유게시판 글 목록 리듀서 액션 생성

--- src/reducers/reducer_board.js
+++ src/reducers/reducer_board.js
@@ -6,6 +6,7 @@ axiosConfig(axios);
 
 // Actions
 const CREATE = 'something-more/board/CREATE';
+const LIST = 'something-more/board/LIST';
 
 // Action Creators
 
@@ -35,6 +36,27 @@ export async function createBoard(formData) {
   }
 }
 
+// 글 목록
+export async function listBoard() {
+
+  let response, error = '';
+
+  try {
+    response = await axios({
+      method: 'get',
+      url: '/board/list/'
+    })
+  } catch (e) {
+    error = e.message;
+  }
+
+  return {
+    type: LIST,
+    response: response,
+    error: error
+  }
+}
+
 // Initial State
 const initialState = {
   list: [],
@@ -47,6 +69,9 @@ export default function reducer(state = initialState, action) {
   switch (action.type) {
     case CREATE:
       return reducerCreateBoard(action, state);
+
+    case LIST:
+      return reducerListBoard(action, state);
 
     default:
       return state;
@@ -65,6 +90,22 @@ function reducerCreateBoard(action, state) {
     return {
       ...state,
       retrieve: {},
+      error: action.error
+    }
+  }
+}
+
+function reducerListBoard(action, state) {
+  if (action.response) {
+    return {
+      ...state,
+      list: action.response.data,
+      error: ''
+    }
+  } else {
+    return {
+      ...state,
+      list: [],
       error: action.error
     }
   }
