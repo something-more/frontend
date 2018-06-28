4c95cd047b38e7118344563791895ce7cd4d6054
huskyhoochu <dfg1499@gmail.com>
2018-06-28 14:32:34 +0900

    내비게이션 컬럼에서 로그인 상태 창 모듈화

--- src/components/dynamic/structure/navigation_column.js
+++ src/components/dynamic/structure/navigation_column.js
@@ -2,54 +2,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { selectAuthor } from '../../../reducers/reducer_author';
-import { signOut } from '../../../reducers/reducer_auth';
-import { decodeJWT } from '../../../include/jwt_decode';
+import AuthenticationState from '../auth/auth_state';
 
 class Navigation extends Component {
-
-  // 인증 상태 렌더링 함수
-  // sessionStorage 에 정보가 하나라도 없으면 로그인 / 회원가입 창을 띄우고
-  // 그렇지 않으면 로그인 되었음을 보여준다
-  renderAuthenticationState() {
-    if (sessionStorage.length === 0) {
-      return (
-      <p>
-        <a href="#login-modal" data-toggle="modal" className="toggle-login">
-          <i className="fa fa-sign-in"/>&nbsp;&nbsp;Sign In</a>
-        <span className="v-divider"/>
-        <a href="#signup-modal" data-toggle="modal" className="toggle-signup">
-          <i className="fa fa-pencil"/>&nbsp;&nbsp;Sign Up</a>
-      </p>)
-    } else {
-      const retrieveData = decodeJWT(sessionStorage.getItem('token'));
-      return (
-          <div className="dropdown">
-            <button className="btn btn-link dropdown-toggle"
-            type="button"
-            id="profileDropdown"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="true">
-              <span className="text-muted">{retrieveData.email}</span>
-              <span className="caret text-muted"/>
-            </button>
-            <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="profileDropdown">
-              {retrieveData.isStaff ? <li><Link to="/me/stories">Stories</Link></li> : ''}
-              <li><Link to="/me/settings">Settings</Link></li>
-              <li role="separator" className="divider"/>
-              <li>
-                <Link
-                  to={'/'}
-                  className="toggle-login"
-                  onClick={() => {this.props.signOut(); window.location.reload();}}>
-                <i className="fa fa-sign-out"/>&nbsp;&nbsp;Sign Out
-              </Link>
-              </li>
-            </ul>
-          </div>
-      )
-    }
-  }
 
   // 필진 리스트 렌더링 함수
   // 리스트를 클릭하면 액션 생성자를 호출해 특정 필진 데이터를 리듀서로 옮긴다
@@ -72,7 +27,7 @@ class Navigation extends Component {
     <div className="nav-col" style={{"border": "none"}}>
       <div id="nav" className="clearfix" role="navigation">
         <div className="user-wrapper">
-          {this.renderAuthenticationState()}
+          <AuthenticationState/>
         </div>
         <ul className="primary-nav">
           <li className="active">
@@ -103,4 +58,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, { selectAuthor, signOut })(Navigation);
+export default connect(mapStateToProps, { selectAuthor })(Navigation);
