8dbf251b89e03d31adc8b3ed842cc3e176de507a
huskyhoochu <dfg1499@gmail.com>
2018-06-28 13:28:05 +0900

    유저 권한 조정 리듀서 액션 생성

--- src/reducers/reducer_admin.js
+++ src/reducers/reducer_admin.js
@@ -6,6 +6,7 @@ axiosConfig(axios);
 
 // Actions
 const LIST = 'something-more/admin/LIST';
+const UPDATE = 'something-more/admin/UPDATE';
 
 // Action Creators
 
@@ -33,6 +34,33 @@ export async function listUsers() {
   }
 }
 
+export async function updateUserAuth(payload) {
+
+  let response, error = '';
+
+  try {
+    response = await axios({
+      method: 'patch',
+      url: `/users/${payload.email}`,
+      headers: {
+        'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+      },
+      data: {
+        is_admin: payload.is_admin,
+        is_staff: payload.is_staff
+      }
+    });
+  } catch (e) {
+    error = e.response.data.message;
+  }
+
+  return {
+    type: UPDATE,
+    response: response,
+    error: error
+  }
+}
+
 // Initial State
 const initialState = {
   list: [],
@@ -45,6 +73,9 @@ export default function reducer(state = initialState, action) {
   switch (action.type) {
     case LIST:
       return reducerListUsers(state, action);
+
+    case UPDATE:
+      return reducerUpdateUserAuth(state, action);
 
     default:
       return state;
@@ -63,6 +94,20 @@ function reducerListUsers(state, action) {
     return {
       ...state,
       list: [],
+      error: action.error
+    }
+  }
+}
+
+function reducerUpdateUserAuth(state, action) {
+  if (!action.error) {
+    return {
+      ...state,
+      error: ''
+    }
+  } else {
+    return {
+      ...state,
       error: action.error
     }
   }
