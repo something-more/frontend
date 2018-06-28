03e46ebfd4974d053da525372a6edc885160b84a
huskyhoochu <dfg1499@gmail.com>
2018-06-28 15:07:48 +0900

    자유게시판 리듀서 생성

---
+++ src/reducers/reducer_board.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { axiosConfig } from '../include/axios';
+
+// axios 설정
+axiosConfig(axios);
+
+// Actions
+
+// Action Creators
+
+// Initial State
+
+// Reducer
+
+// Reducer Functions
