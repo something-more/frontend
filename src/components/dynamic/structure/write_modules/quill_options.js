0118d784eb255cab8a2f7c98c61a41698d49279e
huskyhoochu <dfg1499@gmail.com>
2018-06-30 14:56:00 +0900

    quill option 모듈화 및 재이식

---
+++ src/components/dynamic/structure/write_modules/quill_options.js
@@ -0,0 +1,16 @@
+export default {
+  modules: {
+    toolbar: [
+      [{ size: ['small', false, 'large', 'huge'] }],
+      ['bold', 'italic', 'underline', 'strike'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ script: 'sub' }, { script: 'super' }],
+      [{ indent: '-1' }, { indent: '+1' }],
+      [{ align: [] }],
+      [{ color: [] }, { background: [] }],
+      ['image', 'video', 'link'],
+    ],
+  },
+  placeholder: 'Tell your story...',
+  theme: 'snow',
+}
