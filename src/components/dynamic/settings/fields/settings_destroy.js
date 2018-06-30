9e14f3dc8f60963ddfc0445b60837edd64a24ec9
huskyhoochu <dfg1499@gmail.com>
2018-06-30 14:57:15 +0900

    비밀번호 입력 필드 디렉터리 변경

--- src/components/dynamic/settings/fields/settings_destroy.js
+++ src/components/dynamic/settings/fields/settings_destroy.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { destroyUser } from '../../../../reducers/reducer_auth';
-import PasswordField from '../../structure/password_field';
+import PasswordField from '../../structure/write_modules/password_field';
 
 class DestroyUser extends Component {
 
