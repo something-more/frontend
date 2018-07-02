3c030d8601a69f4b0d8e2d9ceef5acc407259aa3
huskyhoochu <dfg1499@gmail.com>
2018-07-02 22:17:38 +0900

    메인 캐러셀 설계

--- src/components/dynamic/main/carousel.js
+++ src/components/dynamic/main/carousel.js
@@ -1,20 +1,40 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { clientListStory } from '../../../reducers/reducer_story';
+import AlertError from '../structure/alert_error';
 
 class MainCarousel extends Component {
 
+  async componentWillMount() {
+    await this.props.clientListStory();
+  }
+
   renderItem() {
-    return (
-    <div>hello</div>
-    )
+    return _.map(this.props.storyList, (story) => {
+      return (
+        <div key={story.id}>
+          {story.title}
+        </div>
+      )
+    })
   }
 
   render() {
     return (
-    <div className="multi-slider owl-carousel clearfix">
+    <div className="">
       {this.renderItem()}
+      <AlertError errors={this.props.error}/>
     </div>
     )
   }
 }
 
-export default MainCarousel;
+function mapStateToProps(state) {
+  return {
+    storyList: state.story.list,
+    error: state.story.error
+  }
+}
+
+export default connect(mapStateToProps, {clientListStory })(MainCarousel);
