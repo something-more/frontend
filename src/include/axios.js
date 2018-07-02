5e9536f8a5681df451f7f988387635dd050ef934
huskyhoochu <dfg1499@gmail.com>
2018-07-03 01:59:04 +0900

    ESLint fix

--- src/include/axios.js
+++ src/include/axios.js
@@ -1,5 +1,6 @@
 // axios 설정
-export default (axios) => {
+export default (payload) => {
+  const axios = payload;
   axios.defaults.baseURL = 'http://localhost:1323/';
   axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
   axios.defaults.headers.common['Content-Type'] = 'application/json;charset=UTF-8';
