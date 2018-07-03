e19266d79e5426f08bfe76d4e999615008d2bb8e
huskyhoochu <dfg1499@gmail.com>
2018-07-03 13:20:08 +0900

    메인 페이지 공지사항 게시판 생성

--- src/components/dynamic/main/notice_mini.js
+++ src/components/dynamic/main/notice_mini.js
@@ -1,13 +1,77 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import moment from 'moment/moment';
+import { countNotice, listNotice } from '../../../reducers/reducer_notice';
+import AlertError from '../structure/alert_error';
 
 class NoticeMiniList extends Component {
+  async componentWillMount() {
+    await this.props.countNotice();
+    await this.props.listNotice('1&limit=4');
+  }
+
+  renderList() {
+    return _.map(this.props.noticeList, (notice) => {
+      // 글 생성 일자
+      const dateCreated = moment(notice.date_created).format('YYYY-MM-DD');
+      // 글 인덱스 번호
+      const indexNum = (this.props.noticeList.length) - (this.props.noticeList.indexOf(notice));
+      return (
+        <tr key={notice.id}>
+          <td className="col-md-2">
+            {indexNum}
+          </td>
+          <td className="col-md-7">
+            <Link to={`/notice/${notice.id}`}>
+              {notice.title}
+            </Link>
+          </td>
+          <td className="col-md-3">
+            {dateCreated}
+          </td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     return (
       <div className="col-ms-6 col-sm-6">
-notice
+        <h4>
+공지사항
+        </h4>
+        <table className="table table-hover text-center">
+          <thead>
+            <tr>
+              <th className="text-center col-md-2">
+              번호
+              </th>
+              <th className="text-center col-md-7">
+              제목
+              </th>
+              <th className="text-center col-md-3">
+              날짜
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.noticeCount !== 0 ? this.renderList() : null}
+          </tbody>
+        </table>
+        <AlertError />
       </div>
     );
   }
 }
 
-export default NoticeMiniList;
+function mapStateToProps(state) {
+  return {
+    noticeCount: state.notice.count,
+    noticeList: state.notice.list,
+    error: state.notice.error,
+  };
+}
+
+export default connect(mapStateToProps, { countNotice, listNotice })(NoticeMiniList);
