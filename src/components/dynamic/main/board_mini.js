73e4436927a9b1ac4319f211bca523585997b9c3
huskyhoochu <dfg1499@gmail.com>
2018-07-03 23:43:07 +0900

    로딩 컴포넌트 적용

--- src/components/dynamic/main/board_mini.js
+++ src/components/dynamic/main/board_mini.js
@@ -5,11 +5,21 @@ import { Link } from 'react-router-dom';
 import moment from 'moment/moment';
 import { countBoard, listBoard } from '../../../reducers/reducer_board';
 import AlertError from '../structure/alert_error';
+import Loading from '../structure/loading';
 
 class BoardMiniList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
   async componentWillMount() {
     await this.props.countBoard();
     await this.props.listBoard('1&limit=4');
+    await this.setState({ loading: false });
   }
 
   renderList() {
@@ -36,31 +46,42 @@ class BoardMiniList extends Component {
   }
 
   render() {
-    return (
+    const { loading } = this.state;
+    if (!loading) {
+      return (
       <div className="col-ms-6 col-sm-6">
         <h4>
-자유게시판
+          자유게시판
         </h4>
         <table className="table table-hover text-center">
           <thead>
-            <tr>
-              <th className="col-md-2 text-center">
+          <tr>
+            <th className="col-md-2 text-center">
               글쓴이
-              </th>
-              <th className="col-md-8 text-center">
+            </th>
+            <th className="col-md-8 text-center">
               제목
-              </th>
-              <th className="col-md-2 text-center">
+            </th>
+            <th className="col-md-2 text-center">
               날짜
-              </th>
-            </tr>
+            </th>
+          </tr>
           </thead>
           <tbody>
-            {this.props.boardCount !== 0 ? this.renderList() : null}
+          {this.props.boardCount !== 0 ? this.renderList() : null}
           </tbody>
         </table>
         <AlertError errors={this.props.error} />
       </div>
+      );
+    }
+    return (
+    <div className="col-ms-6 col-sm-6">
+      <h4>
+        자유게시판
+      </h4>
+      <Loading/>
+    </div>
     );
   }
 }
