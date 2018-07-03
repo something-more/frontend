9379b573ed5b91bde2e8f4361d760f00185612a4
huskyhoochu <dfg1499@gmail.com>
2018-07-03 17:44:36 +0900

    최근 게시글 리스트 생성

--- src/components/static/footer_column.js
+++ src/components/static/footer_column.js
@@ -1,88 +1,86 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { clientListStory } from '../../reducers/reducer_story';
 
-export default () => (
-  <footer className="footer footer-col clearfix">
-    <div className="footer-content-1">
-      <div className="inner-content">
-        <h5>
-Contact Us
-        </h5>
-        <p className="no-margin-bottom">
-          <strong>
-phone:
-          </strong>
-          {' '}
-          <a className="text-underline" href="tel:8135551234">
-813.555.1234
-          </a>
-          <br />
-          <strong>
-fax:
-          </strong>
-          {' '}
-813.555.1235
-          <br />
-          <span className="overflow">
-            <strong>
-email:
-            </strong>
-            {' '}
-            <a
-              className="text-underline"
-              href="mailto:great-ho@somethingmore.co.kr"
-            >
+class FooterColumn extends Component {
+  async componentWillMount() {
+    await this.props.clientListStory();
+  }
+
+  renderList() {
+    return _.map(this.props.storyList, story => (
+      <li key={story.id}>
+        <Link to={`/stories/${story.id}`}>
+          {story.title}
+        </Link>
+      </li>
+    ));
+  }
+
+  render() {
+    return (
+      <footer className="footer footer-col clearfix">
+        <div className="footer-content-1">
+          <div className="inner-content">
+            <h5>
+            Contact Us
+            </h5>
+            <p className="no-margin-bottom">
+              <strong>
+              phone:&nbsp;
+              </strong>
+              <a className="text-underline" href="tel:8135551234">
+              813.555.1234
+              </a>
+              <br />
+              <strong>
+              fax:&nbsp;
+              </strong>
+            813.555.1235
+              <br />
+              <span className="overflow">
+                <strong>
+                  email:&nbsp;
+                </strong>
+                <a
+                  className="text-underline"
+                  href="mailto:great-ho@somethingmore.co.kr"
+                >
 great-ho@somethingmore.co.kr
-            </a>
-          </span>
-        </p>
-      </div>
-    </div>
-    <hr className="divider" />
-    <div className="footer-content-2">
-      <div className="inner-content">
-        <h5>
-Latest Articles
-        </h5>
-        <ul className="article-list">
-          <li>
-            <a href="your-link-goes-here.html">
-We will amplify our power to benchmark
-            </a>
-          </li>
-          <li>
-            <a href="your-link-goes-here.html">
-We have revolutionized the conceptualization of structuring
-            </a>
-          </li>
-          <li>
-            <a href="your-link-goes-here.html">
-Everything is relative; relatively speaking.
-            </a>
-          </li>
-          <li>
-            <a href="your-link-goes-here.html">
-My other body is an immortal robot body.
-            </a>
-          </li>
-          <li>
-            <a href="your-link-goes-here.html">
-I don&apos;t know who they are but they&apos;re right behind you
-            </a>
-          </li>
-          <li>
-            <a href="your-link-goes-here.html">
-For those about to rock, we salute you
-              {' '}
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-    <hr className="divider" />
-    <div className="inner-content copyright">
-      <p className="small">
-© Copyright 2018 Something More Literature. All rights reserved.
-      </p>
-    </div>
-  </footer>
-);
+                </a>
+              </span>
+            </p>
+          </div>
+        </div>
+        <hr className="divider" />
+        <div className="footer-content-2">
+          <div className="inner-content">
+            <h5>
+            Latest Articles
+            </h5>
+            <ul className="article-list">
+              {this.renderList()}
+            </ul>
+          </div>
+        </div>
+        <hr className="divider" />
+        <div className="inner-content copyright">
+          <p className="small">
+          © Copyright 2018 Something More Literature. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    storyList: state.story.list,
+    error: state.story.error,
+  };
+}
+
+export default connect(mapStateToProps, { clientListStory })(FooterColumn);
