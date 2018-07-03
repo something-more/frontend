b45c72c02960631388f77ab57492f5f90b2daa1e
huskyhoochu <dfg1499@gmail.com>
2018-07-03 14:56:35 +0900

    Base64 함수를 사용하여 토큰 디코딩

--- src/include/jwt_decode.js
+++ src/include/jwt_decode.js
@@ -1,7 +1,9 @@
+import { Base64 } from 'js-base64';
+
 // 사용자의 JWT 토큰을 해석하는 전역 헬퍼 함수
 export default function decodeJWT(token) {
   // 전체 토큰에서 payload 부분만 잘라낸다
   const splitToken = token.split('.')[1];
   // base64로 인코딩된 payload 를 string 으로 디코딩
-  return JSON.parse(window.atob(splitToken));
+  return JSON.parse(Base64.decode(splitToken));
 }
