b798b0e9cb053af67bd2a8eebbc6a755795c6e2e
huskyhoochu <dfg1499@gmail.com>
2018-07-05 14:42:04 +0900

    사용되지 않는 메서드 삭제

--- src/include/render_quill_object.js
+++ src/include/render_quill_object.js
@@ -1,14 +1,3 @@
-export function renderQuillObject(delta, quill) {
-  const contents = JSON.parse(delta);
-  quill.setContents(contents);
-  document.getElementById('content').innerHTML = quill.root.innerHTML;
-  const images = document.getElementById('content').querySelectorAll('img');
-  images.forEach((img) => {
-    img.style.maxWidth = '100%';
-    img.style.height = 'auto';
-  });
-}
-
 export function renderQuillPatchObject(props, value, quill) {
   const content = JSON.parse(value.content);
   quill.setContents(content);
