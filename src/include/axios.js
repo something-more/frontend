89034ff33b682ba265c028a74ed588afadcd6fab
huskyhoochu <dfg1499@gmail.com>
2018-07-07 00:23:42 +0900

    api 엔드포인트 변경

--- src/include/axios.js
+++ src/include/axios.js
@@ -1,7 +1,7 @@
 // axios 설정
 export default function axiosConfig(payload) {
   const axios = payload;
-  axios.defaults.baseURL = 'http://localhost:1323/';
+  axios.defaults.baseURL = 'http://api.somethingmore.co.kr/';
   axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
   axios.defaults.headers.common['Content-Type'] = 'application/json;charset=UTF-8';
   axios.defaults.xsrfHeaderName = 'X-CSRF-Token';
