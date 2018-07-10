d03b5419a93a91026c69b3f78c22dabc8d4e0bde
huskyhoochu <dfg1499@gmail.com>
2018-07-10 16:09:01 +0900

    메인 배너 한글화

--- src/components/dynamic/main/banner.js
+++ src/components/dynamic/main/banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => (
   <div id="banner-title">
     <h1 className="text-center">
-literature beyond novel
+<strong>소설이 아닌, 소설 너머의 문학. 섬띵모어.</strong>
     </h1>
   </div>
 );
