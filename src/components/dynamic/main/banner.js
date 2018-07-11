90808f83d93a2dae0824e4d12d3b3009ce5a70d8
huskyhoochu <dfg1499@gmail.com>
2018-07-11 14:10:48 +0900

    전역 한글 폰트 나눔고딕 적용 & 메인 배너 반응형으로

--- src/components/dynamic/main/banner.js
+++ src/components/dynamic/main/banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => (
   <div id="banner-title">
     <h1 className="text-center">
-<strong>소설이 아닌, 소설 너머의 문학. 섬띵모어.</strong>
+소설이 아닌, 소설 너머의 문학. 섬띵모어.
     </h1>
   </div>
 );
